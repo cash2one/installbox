3d56af0b6a2909232652093f125bea3e3bc674f5
playcrab <jiayuguang@qq.com>
2016-07-20 17:35:37 +0800

    xin

--- z_apps/app/views.py
+++ z_apps/app/views.py
@@ -100,8 +100,8 @@ class CollectionView(TemplateView):
     template_name="z_app/ip_list.html"
 
     def __init__(self):
-        self.FileAdress="/home/itcast/PlayCrab/cangjingge_admin/static/SomeFiles/korea.yaml"
-        self.File_gamename = "/home/itcast/PlayCrab/cangjingge_admin/static/SomeFiles/game_category.txt"
+        self.FileAdress="/static/SomeFiles/korea.yaml"
+        self.File_gamename = "/static/SomeFiles/game_category.txt"
         self.Fd=FileDetach()
 
     def TakeMsg(self):
@@ -143,7 +143,7 @@ class NodeView(TemplateView):
     template_name = "z_app/node.html"
 
     def __init__(self):
-        self.FileAdress = "/home/itcast/PlayCrab/cangjingge_admin/static/SomeFiles/DataSome.yaml"
+        self.FileAdress = "/static/SomeFiles/DataSome.yaml"
 
 
     def TakeMsg(self):
@@ -182,7 +182,7 @@ class NodeAdd(FormView):
 
     def __init__(self):
         self.Fd=FileDetach()
-        self.FileName="/home/itcast/PlayCrab/cangjingge_admin/static/SomeFiles/DataSome.yaml"
+        self.FileName="/static/SomeFiles/DataSome.yaml"
         self.zhPattern = re.compile(u'[\u4e00-\u9fa5]+')
 
     def check_contain_chinese(self,check_str):
@@ -232,7 +232,7 @@ class DeployView(TemplateView):
     template_name = "z_app/deploy.html"
 
     def __init__(self):
-        self.File_gamename = "/home/itcast/PlayCrab/cangjingge_admin/static/SomeFiles/game_category.txt"
+        self.File_gamename = "/static/SomeFiles/game_category.txt"
         self.Fd = FileDetach()
         self.Fn_some = self.Fd.ReadFile(self.File_gamename)[:-1]
         self.GameCategary = ''
